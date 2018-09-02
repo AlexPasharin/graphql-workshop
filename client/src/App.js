62b90c32d852010aa54430c98a012fa4f5706596
lam-ngo <lam.ngo.qu@gmail.com>
2018-09-03 00:12:22 +0300

    Create App.js to manage all containers

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+import BookListContainer from "./containers/BookListContainer";
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:3003/graphql"
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <h1>Graphql book store</h1>
+        <BookListContainer />
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
