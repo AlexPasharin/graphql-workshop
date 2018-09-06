d3b1520d4e602c70950d0b743fd8be85ca769ece
lam-ngo <lam.ngo.qu@gmail.com>
2018-09-06 20:40:29 +0300

    Remove 'age' from author query in BookDetails.js

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { gql } from 'apollo-boost';
-import { graphql } from 'react-apollo';
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
 
 const getBookQuery = gql`
-  query getBook($id: ID){
+  query getBook($id: ID) {
     book(id: $id) {
       id
       title
@@ -12,7 +12,6 @@ const getBookQuery = gql`
       author {
         id
         name
-        age
         books {
           title
           id
@@ -23,37 +22,37 @@ const getBookQuery = gql`
 `;
 
 class BookDetails extends React.Component {
-  render () {
-    const {data} = this.props;
+  render() {
+    const { data } = this.props;
 
     // if query is not made because of skip parameter, "data" props is undefined
     if (!data || !data.book)
-      return (<div id="book-details">No book selected...</div>);
+      return <div id="book-details">No book selected...</div>;
 
-    const {title, genre, price, author} = data.book;
+    const { title, genre, price, author } = data.book;
 
     return (
       <div id="book-details">
         <h3>{title}</h3>
-          <p>Genre: {genre}</p>
-          <p>Price: €{price}</p>
-          <p>Author: {author.name}</p>
-          <p>All books by this author:</p>
-          <ul className="other-books">
-            {author.books.map(b => (
-              <li key={b.id}>{b.title}</li>
-            ))}
-          </ul>
+        <p>Genre: {genre}</p>
+        <p>Price: €{price}</p>
+        <p>Author: {author.name}</p>
+        <p>All books by this author:</p>
+        <ul className="other-books">
+          {author.books.map(b => (
+            <li key={b.id}>{b.title}</li>
+          ))}
+        </ul>
       </div>
     );
   }
-};
+}
 
 export default graphql(getBookQuery, {
   options: props => ({
-      variables: {
-          id: props.bookId
-      },
+    variables: {
+      id: props.bookId
+    }
   }),
   // skip: don't query if props.bookId is null
   skip: props => !props.bookId
