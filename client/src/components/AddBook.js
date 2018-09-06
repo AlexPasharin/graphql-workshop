c2394872813f331ec6ca70eefcfd53e7f505fc5e
lam-ngo <lam.ngo.qu@gmail.com>
2018-09-07 00:31:04 +0300

    Add Mutation method to add book and clear input fields

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,84 +1,89 @@
-import React from 'react';
-import { gql } from 'apollo-boost';
-import { graphql } from 'react-apollo';
+import React from "react";
+import { graphql, compose } from "react-apollo";
+import { getAuthorsQuery, addBookMutation } from "../queries";
 
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
-  }
-`;
 class AddBook extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       name: "",
       genre: "",
       price: NaN,
-      authorId: "",
-    }
+      authorId: ""
+    };
   }
 
   displayAuthors = () => {
-    const {data} = this.props;
+    const { getAuthorsQuery } = this.props;
 
-    if (data.loading) return null;
+    if (getAuthorsQuery.loading) return null;
 
-    return data.authors.map(author => (
-      <option
-        key={author.id}
-        value={author.id}
-      >
+    return getAuthorsQuery.authors.map(author => (
+      <option key={author.id} value={author.id}>
         {author.name}
       </option>
-    )
-  );
-  }
+    ));
+  };
 
   submitForm = e => {
     e.preventDefault();
     console.log(this.state);
-  }
-
+    this.props.addBookMutation({
+      variables: {
+        id: "10",
+        title: this.state.name,
+        genre: this.state.genre,
+        price: this.state.price,
+        authorId: this.state.authorId
+      }
+    });
+    this.setState({
+      name: "",
+      genre: "",
+      price: NaN,
+      authorId: ""
+    });
+  };
 
   render() {
     return (
       <form id="add-book" onSubmit={this.submitForm}>
-          <div className="field">
-              <label>Book name:</label>
-              <input
-                type="text"
-                onChange={e => this.setState({name: e.target.value})}
-              />
-          </div>
-          <div className="field">
-              <label>Genre:</label>
-              <input
-                type="text"
-                onChange={e => this.setState({genre: e.target.value})}
-              />
-          </div>
-          <div className="field">
-              <label>Price:</label>
-              <input
-                type="number"
-                step="0.01"
-                onChange={e => this.setState({price: parseFloat(e.target.value)})}
-              />
-          </div>
-          <div className="field">
-              <label>Author:</label>
-              <select onChange={e => this.setState({authorId: e.target.value})}>
-                  <option value="">Select author</option>
-                  {this.displayAuthors()}
-              </select>
-          </div>
-          <button>+</button>
-       </form>
+        <div className="field">
+          <label>Book name:</label>
+          <input
+            type="text"
+            onChange={e => this.setState({ name: e.target.value })}
+          />
+        </div>
+        <div className="field">
+          <label>Genre:</label>
+          <input
+            type="text"
+            onChange={e => this.setState({ genre: e.target.value })}
+          />
+        </div>
+        <div className="field">
+          <label>Price:</label>
+          <input
+            type="number"
+            step="0.01"
+            onChange={e => this.setState({ price: parseFloat(e.target.value) })}
+          />
+        </div>
+        <div className="field">
+          <label>Author:</label>
+          <select onChange={e => this.setState({ authorId: e.target.value })}>
+            <option value="">Select author</option>
+            {this.displayAuthors()}
+          </select>
+        </div>
+        <button>+</button>
+      </form>
     );
   }
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+  graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
+  graphql(addBookMutation, { name: "addBookMutation" })
+)(AddBook);
